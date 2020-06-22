4430dff07a0b876228376f2dd191678482c39539
r3evans <r3evans@gmail.com>
2020-06-22 13:18:38 -0400

    Commit all

---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int Rating { get; set; }
+    }
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+
+    }
+
+}
